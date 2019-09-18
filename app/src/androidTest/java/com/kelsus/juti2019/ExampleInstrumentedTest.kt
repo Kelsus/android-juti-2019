af6adf0f363d4f0060a0a31b8cf43067dd92546b
Jerónimo Sodero <jeronimo@kelsus.com>
2019-09-18 17:47:30 -0300

    example tests

--- app/src/androidTest/java/com/kelsus/juti2019/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kelsus/juti2019/ExampleInstrumentedTest.kt
@@ -1,24 +1,30 @@
 package com.kelsus.juti2019
 
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.rule.ActivityTestRule
+import com.kelsus.juti2019.view.MainActivity
+import org.hamcrest.Matchers.not
+import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-@RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @Rule
+    @JvmField
+    val activityTestRule = ActivityTestRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.kelsus.juti2019", appContext.packageName)
+    fun isDisplayedTest() {
+        onView(withId(R.id.getGifButton)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun buttonDisabledTest() {
+        val onButton = onView(withId(R.id.getGifButton))
+        onButton.perform(click())
+        onButton.check(matches(not(isEnabled())))
     }
 }
